7bf56c7030bad33d34efb17b045b2d2c274f1502
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-03 10:47:13 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # taobao-coupon-mobile
 taobao-coupon-mobile
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# 生成挂架屏json
+webpack --config ./webpack.skeleton.conf.js
+
+# 骨架屏注入
+node skeleton.js
+
+#注意，作为模板的html文件，需要在被写入内容的位置添加<!--vue-ssr-outlet-->占位符，本例子在div#app里写入：
+
+<div id="app">
+ <!--vue-ssr-outlet-->
+</div>
+```
+
+## 请在谷歌网络浏览器下预览，解决axios跨域问题请在后端配置   windows谷歌浏览器属性目标后添加 --user-data-dir="c:\ChromeDebug" --test-type --disable-web-security
+## 请在谷歌网络浏览器下预览，解决axios跨域问题请在后端配置   mac 命令行打开谷歌浏览器open -n /Applications/Google\ Chrome.app/ --args --disable-web-security  --user-data-dir=自定义存储位置
+###部分截图事例
---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastclick'
+import vueAwesomeSwiper from 'vue-awesome-swiper'
+import VueLazyload from 'vue-lazyload'
+import store from './store'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+Vue.use(vueAwesomeSwiper)
+Vue.use(VueLazyload, {
+  preLoad: 1, // 预加载高度的比例
+  error: 'https://img.alicdn.com/imgextra/i1/790237325/O1CN01zS031123ytm0PzAmE_!!790237325.png?t=1585879843000', // 图像的src加载失败
+  loading: 'https://img.alicdn.com/imgextra/i1/790237325/O1CN01zS031123ytm0PzAmE_!!790237325.png?t=1585879843000', // src的图像加载
+  attempt: 1, // 尝试计数
+  listenEvents: [ 'scroll', 'mousewheel', 'touchmove' ] // 你想要监听的事件
+}) 
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
