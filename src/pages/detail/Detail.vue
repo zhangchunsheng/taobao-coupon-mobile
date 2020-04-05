c69bdaf8d7f8627750a6d4a104ea94d0ed991fb6
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-05 13:27:01 +0800

    update

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -233,7 +233,7 @@ export default {
                     color: $RedColor
             .quan-info
                 width: 100%
-                background: url('./goods_quan.png') no-repeat
+                background: url('https://img.alicdn.com/imgextra/i3/790237325/O1CN01cgLnmf23ytm1x4ZzQ_!!790237325.png?t=1586064316000') no-repeat
                 background-size: auto 100%;
                 height: 0
                 padding-bottom: 23.897%
