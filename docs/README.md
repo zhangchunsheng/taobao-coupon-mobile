8a2a77e51565b17847c0e02e9ade43b35eadcc44
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-03-10 19:04:42 +0800

    update background

---
+++ docs/README.md
@@ -0,0 +1,3 @@
+```css
+background: url('https://img.alicdn.com/imgextra/i3/790237325/O1CN01cgLnmf23ytm1x4ZzQ_!!790237325.png?t=1586064316000') no-repeat
+```
--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -248,7 +248,7 @@ export default {
                     color: $RedColor
             .quan-info
                 width: 100%
-                background: url('https://img.alicdn.com/imgextra/i3/790237325/O1CN01cgLnmf23ytm1x4ZzQ_!!790237325.png?t=1586064316000') no-repeat
+                background-color: #fe1335
                 background-size: auto 100%;
                 height: 0
                 padding-bottom: 23.897%
